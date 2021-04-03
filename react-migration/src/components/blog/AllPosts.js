5df65fcbe218382b819c5b9bb1c844afb91961ac
code-c <ccottrel@uvm.edu>
2021-04-02 21:43:31 -0400

    finalized blog layout for dev

--- react-migration/src/components/blog/AllPosts.js
+++ react-migration/src/components/blog/AllPosts.js
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../../client";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 export default function AllPosts() {
   const [allPostsData, setAllPosts] = useState(null);
@@ -30,28 +32,26 @@ export default function AllPosts() {
 						<h2 class="display text-center headings">Blog</h2>
       </div>
 
-      <div class="container-fluid padding blog-layout">
+      <div class="container-fluid padding card-layout">
         {/* <div class="row padding blog-layout"> */}
           {allPostsData &&
             allPostsData.map((post, index) => (
-              <div class="col-md-4">
-                <div class="card">
-                  <Link to={"/" + post.slug.current} key={post.slug.current}>
-                    <span key={index}>
-                      <img src={post.mainImage.asset.url} alt="" />
-                      <span>
-                        <div class="card-body">
-                          <h4 class="card-title">{post.title}</h4>
-                          <button class="btn btn-outline-secondary hvr-grow">See Post</button>
-                        </div>
-                      </span>
+              <Link to={"/" + post.slug.current} key={post.slug.current}>
+                {/* card n link to make whole div clickable */}
+                <Card>
+                  <span key={index}>
+                    <Card.Img src={post.mainImage.asset.url} alt="" />
+                    <span>
+                      <Card.Body>
+                        <Card.Title>{post.title}</Card.Title>
+                        <Button class="btn btn-outline-secondary hvr-grow">See Post</Button>
+                        </Card.Body>
                     </span>
-                  </Link>
-                </div>
-              </div>
+                  </span>
+                </Card>
+              </Link>
             ))}
         </div>
       </div>
-    // </div>
   );
 }
